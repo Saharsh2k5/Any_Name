d49f6febf6f1523fbe5ece6d4e49b470b0e08a12
Burra Saharsh <143330102+Saharsh2k5@users.noreply.github.com>
2025-08-07 19:24:56 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,39 +1,38 @@
 import random
 import time
 
-operators=['+','-','*']
-min_operand=3
-max_operand=12
-total_problems=0
+operators = ['+', '-', '*']
+MIN_OPERAND = 3
+MAX_OPERAND = 12
+TOTAL_PROBLEMS = 0
 
 def generate_problem():
-    left=random.randint(min_operand,max_operand)
-    right=random.randint(min_operand,max_operand)
-    operation=random.choice(operators)
-    
-    expr=str(left)+" "+operation+" "+str(right)
-    answer=eval(expr)
-    return expr,answer
-    
-wrong=0
+    left = random.randint(MIN_OPERAND, MAX_OPERAND)
+    right = random.randint(MIN_OPERAND, MAX_OPERAND)
+    operation = random.choice(operators)
+    expression = str(left) + " " + operation + " " + str(right)
+    result = eval(expression)
+    return expression, result
+
+WRONG = 0
 input("Press enter to start")
-total_problems=int(input("How many problems you want to try?"))
+TOTAL_PROBLEMS = int(input("How many problems you want to try?"))
 print("Test Started ALL THE BEST!!")
 print("**************************")
 
-start=time.time()
-for i in range(total_problems):
-    expr,answer=generate_problem()
-    guess=input("Problem #"+str(i+1)+":"+expr+" = ")
-    if(guess==str(answer)):
+start = time.time()
+for i in range(TOTAL_PROBLEMS):
+    expr, answer = generate_problem()
+    guess = input("Problem #" + str(i + 1) + ":" + expr + " = ")
+    if guess == str(answer):
         print("Answered Correctly, done with testing")
         break
     print("Try and Try till you get right!")
-    wrong+=1
-          
-end_time=time.time()
-total_time=end_time-start
+    WRONG += 1
 
-print("You answered wrongly",wrong,"questions")
-print("Keep UP the good work, you finished in",total_time,"seconds!")
+end_time = time.time()
+total_time = end_time - start
+
+print("You answered wrongly", WRONG, "questions")
+print("Keep UP the good work, you finished in", total_time, "seconds!")
 print("**************************")
